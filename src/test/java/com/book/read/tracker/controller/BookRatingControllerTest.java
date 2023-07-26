ea12f063e11aa57b139380e51d805b274ee1566e
Justin-mario <79143260+Justin-mario@users.noreply.github.com>
2023-07-26 23:34:34 +0100

    added new controller test

---
+++ src/test/java/com/book/read/tracker/controller/BookRatingControllerTest.java
@@ -0,0 +1,17 @@
+package com.book.read.tracker.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+
+
+@WebMvcTest
+class BookRatingControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+}
